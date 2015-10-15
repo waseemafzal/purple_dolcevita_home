55f509b9b50d64592343c224cdb0f4f3c2079f2b
waseem afzal <waseemafzal31@gmail.com>
2015-10-15 15:57:06 +0500

    home index page
    index page

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php require_once"main/header.php" ?>
+    <!-- -------Navbar ends -->
+    <!------container starts ----->
+        <div class="container margin0auto">
+             <div class="row">
+                      <video id="myVideo" class="col-xs-12" muted loop   autoplay poster="images/poster.PNG" preload="auto">
+<!--                        <source src="http://purpleartsmedia.com/staging/dolcevita/option/5/videos/Third.mp4" type="video/mp4">
+-->                     </video>
+					 <div class="col-xs-12 col-md-8 col-md-offset-8 diamond_img_wrap">
+					 <img  id="diamond_img" src="images/Diamond-logo.png">
+					 
+					 </div>
+					
+                </div>
+<!------------------------------------ Products rows Start -------------------------------->
+            <div class="row container">
+                <div class="col-xs-12 col-sm-12 padding0">
+                    <img class=" product_img" src="images/text.png">
+                    <div class="unique-product">
+                        <img class="product_img" src="images/home/emancipate_your_love.png">
+                        <div class="home-overlay-txt-box emancipate">
+                            <img class="home-overlay-txt" src="images/home/txt-emancipate.png">
+                            <a href="" > <img class="img-readmore" src="images/home/read_more.png"></a>
+                        </div>
+                    </div>
+                    
+                    <div class="product_div">
+                        <img class="product_img" src="images/home/img-we-done-make-furniture.png">
+                        <div class="home-overlay-txt-box furniture">
+                            <img class="home-overlay-txt" src="images/home/txt-we-done-make-furniture.png">
+                            <a href="" > <img class="img-readmore" src="images/home/read_more.png"></a>
+                        </div>
+                    </div>
+                    
+                    <div class="product_div">
+                        <img class="product_img" src="images/home/img-nouveau.png">
+                        <div class="home-overlay-txt-box nouveau">
+                            <img class="home-overlay-txt" src="images/home/txt-nouveau.png">
+                            <a href="" > <img class="img-readmore" src="images/home/read_more.png"></a>
+                        </div>
+                    </div>
+                    
+                    <div class="product_div">
+                        <img class="product_img" src="images/home/img_B&B.png">
+                        <div class="home-overlay-txt-box bold_beauty">
+                            <img class="home-overlay-txt" src="images/home/txt_B&B.png">
+                            <a href="" > <img class="img-readmore" src="images/home/read_more.png"></a>
+                        </div>
+                    </div>
+                    
+                    <div class="product_div">
+                        <img class="product_img" src="images/home/img_watch_the_video.png">
+                        <div class="home-overlay-txt-box watch_video">
+                            <img class="home-overlay-txt" src="images/home/txt_watch_the_video.png">
+                            <a id="btn-video-play" href="http://player.vimeo.com/video/17914974" target="_blank"><img id="img-play-back" class="img-readmore" src="images/home/video-play-button_2x.png"></a>
+                        </div>
+                    </div>
+                    <!--<div class="product_div" id="VIDEO_playback">
+                     <ul class="bxslider-video">
+  <li>
+    <iframe src="http://player.vimeo.com/video/17914974" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+  </li>
+  <li><img src="/images/730_200/houses.jpg" /></li>
+</ul>
+                    </div>-->
+                    
+                      
+ 
+                    
+                </div>
+            </div>
+<!------------------------------------ FOOTER START-------------------------------->
+<?php require_once"main/footer.php" ?>
