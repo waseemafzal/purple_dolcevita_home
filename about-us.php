f84fdeba70dc63a80983500314d13bfb4423e292
waseem afzal <waseemafzal31@gmail.com>
2015-10-15 16:03:45 +0500

    js
    js changes

---
+++ about-us.php
@@ -0,0 +1,37 @@
+<?php require_once"main/header.php" ?>
+    <!-- -------Navbar ends -->
+    <!------container starts ----->
+        <div class="container margin0auto">
+            <div class="row">
+                <div>
+                    <img class="col-xs-12"src="images/about-us/slider.png">
+                </div>
+            </div>
+            <div class="container">
+                <div class=" heading textalcenter margin0auto">
+                    <h3 class="dolcevita-heading" >
+                        D O L C E <span class="vita">V I T A </span> H O M E
+                    </h3>    
+                    <div class="underline-34"></div>    
+                </div>
+                <div class="margin0auto description textalcenter col-xs-12 col-sm-12">
+                    <p>
+                        Contrary to popular belief, lorem Ipsum is not simply random text. It has roots in a piece of classical Latin Liiterature from 45 BC, making it over 2000 years old.
+                        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure latin words, consectetur, from a lorem ipsum passage, and going through the cites of the wod in clssical literature discovered the undoubtable source.
+                        lorem ipsum compes from section 1.10.32 and 1.10.33 of "die finibus bonorum et malorum" (the extreme of good and evil) by cicero, written in 45 BC.
+                        this book is treatise on the throty of ethics, very popular during the Renais-sance.
+                        the first line of lorem ipsum, "lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                        <br><br>
+                        the standard chunk of lorem ipsum used since the 1500s is reproduced below for those interested.
+                        Sections 1.10.32 and 1.10.33 from "de finibus bonorum et malorum" by cicero are also reproduced in their exact original from, accoumpanied by english version
+                        from the 1014 translation by H. Rackham.
+                    </p>
+                </div>
+            </div>
+            <!--             ...container ends -->
+
+
+
+
+            <!------------------------------------ FOOTER START-------------------------------->
+<?php require_once"main/footer.php" ?>
